284a1254b48b6828bad47b51278547bfb0a4eb5d
Vishal Mali <vishalmali06@gmail.com>
2025-06-05 19:07:40 +0530

    Write a function that accepts a number and checks whether it is even or odd.
    If the number is divisible by 2 (i.e., remainder is 0), it’s an Even number.
    
    Otherwise, it’s an Odd number.
    
    Test the function with inputs 18 and 5.

---
+++ arrays/evenOddNumber.js
@@ -0,0 +1,11 @@
+function EvenOddCheck(num) {
+  let rem = num % 2;
+  if (rem === 0) {
+    console.log("Enven Number");
+  } else {
+    console.log("Odd number");
+  }
+}
+
+EvenOddCheck(10);
+EvenOddCheck(11);
